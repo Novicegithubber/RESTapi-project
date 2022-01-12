d1808d1888fd146e2cab1d53e463d42058aaa748
Mahmoud <mahmoudmagdymail@gmail.com>
2022-01-12 12:14:09 +0200

    project

---
+++ server.js
@@ -0,0 +1,49 @@
+const app = require("express")();
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+const port = 3000;
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+
+const dbURI =
+  "mongodb+srv://Mahmoudmagdy:qweasdzxc123@project.yngmc.mongodb.net/restapi?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port);
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("RESTapi project");
+});
+
+app.get("/users", (req, res) => {
+  User.find({}, function (err, user) {
+    res.send(user);
+  });
+});
+
+app.post("/users", (req, res) => {
+  User.create(req.body).then(function (user) {
+    res.send(user);
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  User.findOneAndUpdate(
+    { _id: "61dea32356777c64844e7d13" },
+    { name: "ahmed" },
+    function (err, user) {
+      res.send(user);
+    }
+  );
+});
+
+app.delete("/users/:id", (req, res) => {
+  User.findByIdAndRemove(
+    { _id: "61dea32356777c64844e7d13" },
+    function (err, user) {
+      res.send(user);
+    }
+  );
+});
